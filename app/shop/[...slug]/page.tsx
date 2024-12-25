7aa098295e2d019298bbae19eb3b645ca79a19c0
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2024-12-25 22:19:36 +0600

    slug array

--- app/shop/[...slug]/page.tsx
+++ app/shop/[...slug]/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const ShopArea = ({ params }: { params: { slug: string } }) => {
+    console.log(params.slug)
     return (
         <div>
             <p>All the segment {params.slug} </p>
+            <p>All the segment {params.slug[4]} slug is is array </p>
         </div>
     )
 }
