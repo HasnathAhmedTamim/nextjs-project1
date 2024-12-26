3546ae326a3acbe6cc7d2c7acf45313b1b6394d5
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2024-12-26 18:28:51 +0600

    link component used  and anchor difference

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <div className="">
       <h2>Hello Home Page</h2>
-
+      <Link href="/blog">Go to blog</Link>
+      <Link href="/product">Go to product page</Link>
     </div>
   );
 }
